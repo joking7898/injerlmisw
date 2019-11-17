c315e2e4bf62f2541410e61b025c995f06e89656
yunu95 <liunux95@gmail.com>
2019-11-17 22:13:57 +0900

    동적 페이지생성 가능

--- PageRouter.js
+++ PageRouter.js
@@ -2,7 +2,8 @@ var express = require('express')
 var router = express.Router()
 var mysql = require('mysql');
 var ejs = require('ejs');
-
+var qs = require('querystring')
+var url = require('url')
 //밑 코드 데이터베이스 연결
 var mysqlcon = mysql.createConnection({
     host: 'gottraction.c9jcx2tgvrrn.us-west-2.rds.amazonaws.com',
@@ -17,15 +18,23 @@ var bodyparser = require('body-parser')
 router.use(bodyparser.urlencoded({extended:false}))
 
 mysqlcon.connect(function(err){
-    console.log("Ekrqnr", err);
 })
 
 //페이지 출력 여기서 해주라는 요청.
 router.get("/views/listings.ejs",function (req,res){
-    mysqlcon.query("select * from attraction",function(err,results) {
+    var querydata = url.parse(req.url,true).query;
+    
+    console.log(querydata.category)
+    var category = (querydata.category == undefined)?"":querydata.category;
+    var location = (querydata.location == undefined)?"서울시":querydata.location;
+    var querystring = "select * from attraction where location = '"+location+"'";
+    if(category !="")
+        querystring += "and category = '"+category+"'";
+
+    mysqlcon.query(querystring,function(err,results) {
     if (!err){
        // console.log('The solution is: ', rows);
-       // log로 체크하는구문.
+       // log로 체크하는구문.   
         res.render('listings.ejs', {
         result: results    
         // SQL Query 실행결과인 results 를 statusList.ejs 파일에 result 이름의 리스트로 전송
@@ -36,6 +45,9 @@ router.get("/views/listings.ejs",function (req,res){
     }
     })
     //res.redirect("/views/listing.ejs")
+})
+router.get("/views/CategorySelection",function (req,res){
+    res.redirect("/views/index.ejs")
 })
 /**
  * 홈페이지에 url없이 접속시 index url로 리다이렉트
