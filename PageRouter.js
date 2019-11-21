6cec787e37fdf37784a993baaa1554d59b69f1e1
joking7898 <joking78981@gmail.com>
2019-11-21 17:43:21 +0900

    db접속 url 변경

--- PageRouter.js
+++ PageRouter.js
@@ -7,7 +7,7 @@ var url = require('url')
 
 //밑 코드 데이터베이스 연결
 var mysqlcon = mysql.createConnection({
-    host: 'gottraction.c9jcx2tgvrrn.us-west-2.rds.amazonaws.com', user: 'admin', password: 'freehongkong', database: 'gottraction'
+    host: 'yunudb.c9jcx2tgvrrn.us-west-2.rds.amazonaws.com', user: 'admin', password: 'freehongkong', database: 'gottraction'
     //port: '3306'
 });
 var fs = require('fs')
