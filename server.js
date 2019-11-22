64c5d906af4dbcc2eb6c307abac842b63108e9d8
yunu95 <liunux95@gmail.com>
2019-11-23 00:37:41 +0900

    ㅁㅁㅁ

--- server.js
+++ server.js
@@ -50,7 +50,6 @@ Eapp.use(function(request,response){
     response.writeHead(200);
     if(url.endsWith('html'))
         response.redirect(url.slice(0,-4)+'ejs')
-    else
     /*
     홈페이지에 들어온 request의 url에 따라 웹 페이지를 출력함. 원래 이렇게 짜면 안되고, PageRouter에서 출력해 줘야 함.
     */
