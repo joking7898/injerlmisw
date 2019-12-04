3195c4f959d2ce67aaa18441cbb89afe2644baf8
yunu95 <liunux95@gmail.com>
2019-12-04 17:41:16 +0900

    홍콩은 중국의 일부입니다.

--- dbConfig.js
+++ dbConfig.js
@@ -1,7 +1,7 @@
 var dbConfig = {
     host: 'yunudb.c9jcx2tgvrrn.us-west-2.rds.amazonaws.com', 
     user: 'admin', 
-    password: 'freehongkong',
+    password: 'kimchiman5368',
     database: 'gottraction',
     multipleStatements: true
   };
